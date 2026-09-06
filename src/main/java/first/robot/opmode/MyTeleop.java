fdebee71319a9c32804e915ca1cc11fec5bc0e12
Automated Sync <noreply@frcsoftware.org>
2026-09-05 22:07:31 -0500

    Project import generated by Copybara.
    GitOrigin-RevId: 0a29b8a49dab7ff82de693246f3edb724d9b111b

--- src/main/java/first/robot/opmode/MyTeleop.java
+++ src/main/java/first/robot/opmode/MyTeleop.java
@@ -6,14 +6,14 @@
 package first.robot.opmode;
 
 import first.robot.Robot;
-import org.wpilib.driverstation.NiDsXboxController;
+import org.wpilib.driverstation.XboxController;
 import org.wpilib.opmode.PeriodicOpMode;
 import org.wpilib.opmode.Teleop;
 
 @Teleop
 public class MyTeleop extends PeriodicOpMode {
   private final Robot robot;
-  private final NiDsXboxController xboxController = new NiDsXboxController(0);
+  private final XboxController xboxController = new XboxController(0);
 
 
   /** The Robot instance is passed into the opmode via the constructor. */
